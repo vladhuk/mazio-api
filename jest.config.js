489584fc996efa882d1603a689462310b4f82435
vladhuk <vladhookovskiy@gmail.com>
2020-07-12 23:16:07 +0300

    Issue mazio-api#1: Create models - Add config for tests debugging;

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['./tests/setup/index.ts'],
+  modulePathIgnorePatterns: ['./out/', './node_modules/'],
 };
