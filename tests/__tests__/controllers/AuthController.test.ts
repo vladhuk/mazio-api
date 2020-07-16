369fd6b262aa86e434d0f29e9c8e206b69a5ba77
vladhuk <vladhookovskiy@gmail.com>
2020-07-16 22:53:11 +0300

    Issue mazio-api#3: Add api and services - Add AuthController tests;

---
+++ tests/__tests__/controllers/AuthController.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import app from '../../../src/app';
+import User from '../../../src/models/User';
+
+const testUser = { username: 'testusername', password: 'testpassword' };
+
+it('signUp(). When: username and password are correct. Expected: Created user, 200 and correct response body', async () => {
+  const response = await request(app)
+    .post('/api/auth/signup')
+    .send(testUser)
+    .expect(200);
+
+  expect(response.body.tokenType).toBe('Bearer');
+  expect(response.body.token).not.toBeNull();
+  expect(response.body.user).toEqual({ username: testUser.username });
+  expect(await User.exists({ username: testUser.username })).toBeTruthy();
+});
+
+it('signIn(). When: user exists. Expected: 200 and correct response body', async () => {
+  const user = new User(testUser);
+  await user.save();
+
+  const response = await request(app)
+    .post('/api/auth/signin')
+    .send(testUser)
+    .expect(200);
+  expect(response.body.token).not.toBeNull();
+  expect(response.body.user).toEqual({ username: testUser.username });
+});
+
+it('signIn(). When: user does not exist. Expected: 400', () => {
+  return request(app).post('/api/auth/signin').send(testUser).expect(400);
+});
