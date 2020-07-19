c2e4b3f727cd39459783f32cccb5150c665034c8
vladhuk <vladhookovskiy@gmail.com>
2020-07-19 21:47:58 +0300

    Issue mazio-api#3: Add api and services - Bind middlewares on path;

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import authRouter from './auth';
 import usersRouter from './users';
 import mazesRouter from './mazes';
 import roomsRouter from './rooms';
+import { authRequired } from '../middlewares/authentication';
+import userIdChecker from '../middlewares/userIdChecker';
 
 const router = Router();
 
 router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+router.use('/users', [authRequired, userIdChecker], usersRouter);
 router.use('/mazes', mazesRouter);
 router.use('/rooms', roomsRouter);
 
