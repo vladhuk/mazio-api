e952ffe12b689286a7f94307141969415c170477
vladhuk <vladhookovskiy@gmail.com>
2020-07-19 21:48:27 +0300

    Issue mazio-api#3: Add api and services - Add user controller;

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,58 @@
+import { RequestHandler, Response } from 'express';
+import * as userService from '../services/UserService';
+import { Types } from 'mongoose';
+import HttpStatus from 'http-status-codes';
+import UserNotFoundError from '../errors/UserNotFoundError';
+
+function defaultErrorHandler(err: Error, res: Response) {
+  if (err instanceof UserNotFoundError) {
+    return res.status(HttpStatus.NOT_FOUND).send(err.message);
+  }
+  return res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+}
+
+export const getFriends: RequestHandler = (req, res) => {
+  userService
+    .getFriends(Types.ObjectId(req.params.id))
+    .then((friends) => res.json(friends))
+    .catch((err) => defaultErrorHandler(err, res));
+};
+
+export const addFriend: RequestHandler = (req, res) => {
+  userService
+    .addFriend(Types.ObjectId(req.params.id), req.body.username)
+    .then((friends) => res.json(friends))
+    .catch((err) => defaultErrorHandler(err, res));
+};
+
+export const deleteFriend: RequestHandler = (req, res) => {
+  userService
+    .deleteFriend(
+      Types.ObjectId(req.params.id),
+      Types.ObjectId(req.params.friendId)
+    )
+    .catch((err) => defaultErrorHandler(err, res));
+};
+
+export const getIgnoredUsers: RequestHandler = (req, res) => {
+  userService
+    .getIgnoredUsers(Types.ObjectId(req.params.id))
+    .then((ignoredUsers) => res.json(ignoredUsers))
+    .catch((err) => defaultErrorHandler(err, res));
+};
+
+export const addIgnoredUser: RequestHandler = (req, res) => {
+  userService
+    .addIgnoredUser(Types.ObjectId(req.params.id), req.body.username)
+    .then((ignoredUsers) => res.json(ignoredUsers))
+    .catch((err) => defaultErrorHandler(err, res));
+};
+
+export const deleteIgnoredUser: RequestHandler = (req, res) => {
+  userService
+    .deleteIgnoredUser(
+      Types.ObjectId(req.params.id),
+      Types.ObjectId(req.params.ignoredUserId)
+    )
+    .catch((err) => defaultErrorHandler(err, res));
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
+import * as userController from '../controllers/UserController';
 
 const router = Router();
 
-router.get('/:id/friends');
-router.post('/:id/friends');
-router.delete('/:id/friends/:id');
-router.get('/:id/ignored');
-router.post('/:id/ignored');
-router.delete('/:id/ignored/:id');
+router.get('/:id/friends', userController.getFriends);
+router.post('/:id/friends', userController.addFriend);
+router.delete('/:id/friends/:friendId', userController.deleteFriend);
+router.get('/:id/ignored', userController.getIgnoredUsers);
+router.post('/:id/ignored', userController.addIgnoredUser);
+router.delete('/:id/ignored/:ignoredUserId', userController.deleteIgnoredUser);
 
 export default router;
