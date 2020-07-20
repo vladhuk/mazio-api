b4a1cf3b6ba7eb37041f8e70cb1df7b6cd580d9b
vladhuk <vladhookovskiy@gmail.com>
2020-07-20 18:36:05 +0300

    Issue mazio-api#3: Add api and services - Add error;

---
+++ src/errors/MazeNotFoundError.ts
@@ -0,0 +1,7 @@
+import { Types } from 'mongoose';
+
+export default class MazeNotFoundError extends Error {
+  constructor(mazeId: Types.ObjectId) {
+    super(`Maze with id ${mazeId} is not founded.`);
+  }
+}
