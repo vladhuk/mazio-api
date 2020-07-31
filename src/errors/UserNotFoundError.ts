266e879cb9e988a2a2ca21d3c1ac8236092f1125
vladhuk <vladhookovskiy@gmail.com>
2020-07-31 22:52:57 +0300

    Issue mazio-api#3: Add api and services - Fix typo;

--- src/errors/UserNotFoundError.ts
+++ src/errors/UserNotFoundError.ts
@@ -3,9 +3,9 @@ import { Types } from 'mongoose';
 export default class UserNotFoundError extends Error {
   constructor(userId?: Types.ObjectId, username?: string) {
     if (userId) {
-      super(`User with id ${userId} is not founded.`);
+      super(`User with id ${userId} is not found.`);
     } else {
-      super(`User with username ${username} is not founded.`);
+      super(`User with username ${username} is not found.`);
     }
   }
 }
