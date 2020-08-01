f7bdaca3d5595c5e66dd38fb66f8f195434830d1
vladhuk <vladhookovskiy@gmail.com>
2020-08-01 17:31:28 +0300

    Issue mazio-api#3: Add api and services - Refactor; - Add new methods;

--- src/services/MazeService.ts
+++ src/services/MazeService.ts
@@ -1,21 +1,16 @@
 import { Types } from 'mongoose';
 import { Type as MazeType, IMaze } from '../models/Maze';
-import Maze, { defaultPopulateOptions } from '../models/Maze/Maze';
+import Maze, { Type } from '../models/Maze/Maze';
 import User from '../models/User';
 import MazeNotFoundError from '../errors/MazeNotFoundError';
 import UserNotFoundError from '../errors/UserNotFoundError';
+import BadRequestError from '../errors/http/BadRequestError';
 
 export async function getMazeById(id: Types.ObjectId): Promise<IMaze> {
-  const maze = await Maze.findById(id).populate(defaultPopulateOptions);
-
-  if (!maze) {
-    throw new MazeNotFoundError(id);
-  }
-
-  return maze;
+  return Maze.getById(id);
 }
 
-export async function getMazesByOwnerAndType(
+export async function getMazesByOwnerIdAndType(
   ownerId: Types.ObjectId,
   mazeType: MazeType
 ): Promise<IMaze[]> {
@@ -25,9 +20,7 @@ export async function getMazesByOwnerAndType(
     throw new UserNotFoundError(ownerId);
   }
 
-  return Maze.find({ owner: ownerId, type: mazeType })
-    .populate(defaultPopulateOptions)
-    .lean();
+  return Maze.getByOwnerIdAndType(ownerId, mazeType);
 }
 
 export async function createMaze(
@@ -55,10 +48,10 @@ export async function updateMaze(
   mazeId: Types.ObjectId,
   maze: IMaze
 ): Promise<IMaze> {
-  const currentMaze = await getMazeById(mazeId);
+  const currentMaze = await Maze.getById(mazeId, { lean: false });
 
-  if (!currentMaze) {
-    throw new MazeNotFoundError(mazeId);
+  if (currentMaze.type !== Type.DRAFT) {
+    throw new BadRequestError(`Maze ${mazeId} is not a draft.`);
   }
 
   if (maze.title) {
@@ -72,6 +65,24 @@ export async function updateMaze(
   }
 
   return currentMaze.save();
+}
+
+export async function deleteMaze(id: Types.ObjectId): Promise<void> {
+  await Maze.deleteOne({ _id: id });
+}
+
+export async function publishMaze(id: Types.ObjectId): Promise<IMaze> {
+  const draftMaze = await Maze.getById(id);
+
+  if (draftMaze.type !== Type.DRAFT) {
+    throw new BadRequestError(`Maze ${id} is not a draft.`);
+  }
+
+  delete draftMaze._id;
+
+  const publishedMaze = new Maze({ ...draftMaze, type: Type.PUBLISHED });
+
+  return publishedMaze.save();
 }
 
 export async function incrementLikes(
