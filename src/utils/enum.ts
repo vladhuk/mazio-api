89a8e040a4e5e532ad10bb22477087fd342d15a2
vladhuk <vladhookovskiy@gmail.com>
2020-08-12 18:22:51 +0300

    Issue mazio-api#3: Add api and services - Remove enum utils;

--- src/utils/enum.ts
+++
@@ -1,9 +0,0 @@
-export function ordinals<T>(e: { [s: string]: T }): T[] {
-  const values = Object.values(e);
-  return values.slice(values.length / 2);
-}
-
-export function names<T>(e: { [s: string]: T }): T[] {
-  const values = Object.values(e);
-  return values.slice(0, values.length / 2);
-}
