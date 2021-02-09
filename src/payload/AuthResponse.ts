d2ef6fdb6255ff706cf1b13960e63e7bb172bb05
vladhuk <vladhookovskiy@gmail.com>
2021-02-10 00:49:42 +0200

    #3 - Change auth response;

--- src/payload/AuthResponse.ts
+++ src/payload/AuthResponse.ts
@@ -3,10 +3,9 @@ import { IUserDto } from '../models/User';
 export default class AuthResponse {
   private user: IUserDto;
   private token: string;
-  private tokenType = 'Bearer';
 
   constructor(user: IUserDto, token: string) {
     this.user = user;
-    this.token = token;
+    this.token = `Bearer ${token}`;
   }
 }
